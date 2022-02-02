b7ce8194c3f1c3267b01e0680c008bfa49c2fd88
Tanuj Ojha <tanujojha2042@gmail.com>
2022-02-02 11:24:28 +0530

    initial commit-after main page completion

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+// $("a.nav-link").click(function(){
+//   $(this).addClass("active");
+//   // $("a.nav-link").removeClass("active");
+// });
+
+// $("a.nav-link").hover(function(){
+//   $(this).css("color", "yellow");
+//   }, function(){
+//   $(this).css("color", "#D3DEDC");
+// });
