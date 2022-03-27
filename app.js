a807b0655a417f1ea39e6cf728c27b2701a7523f
Tanuj Ojha <tanujojha2042@gmail.com>
2022-03-28 01:00:48 +0530

    just before post authentication rendering

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -7,6 +11,35 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }))
 
 // console.log(express.static("public"));
+
+app.use(session({
+  secret: "this is a secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/ArHubUserDB")
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
@@ -22,15 +55,63 @@ app.get("/signup",function(req,res){
 
 
 app.post("/signup", function(req,res){
-  console.log(req.body);
-  // var email = req.body.email;
-  // var password = req.body.paswd;
-  // console.log(email, password);
-  // res.send("hello thanks")
+  // console.log(req.body);
+
+  //************ ArHubUserDB code for Create Account ***********
+  if (req.body.creaccbtn === "creaccbtn"){
+    console.log("User requested to create account");
+    User.register({username: req.body.email, firstName: req.body.fname, lastName: req.body.lname}, req.body.paswd, function(err, user){
+  // console.log("the user log is : " + user);
+  if(err){
+    console.log(err);
+    res.redirect("/signup");
+  }else{
+    passport.authenticate("local") (req, res, function(){
+      res.redirect("/welcome");
+    });
+
+
+
+  }
+});
+  }
+  //************ ArHubUserDB code for Sign-In ***********
+  else if (req.body.signinbtn){
+    const user = new User({
+
+      username: req.body.email,
+      password: req.body.paswd
+    });
+
+    req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/welcome");
+      });
+    }
+  });
+
+}else{
+  console.log("something went wrong");
+}
+
 });
 
 
 
-app.listen(5000,function(){
+app.get("/welcome",function(req,res){
+  if(req.isAuthenticated()){
+    res.sendFile(__dirname + "/testweb.html");
+  }else{
+    res.redirect("/signup");
+  }
+
+});
+
+
+
+app.listen(3000,function(){
   console.log("server started on 3000");
 })
